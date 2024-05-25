43081e9c1ec163ab094331e95f19d8da45d14345
Andrea <siizx@live.it>
2024-05-25 13:01:32 +0200

    general update

--- dotFilesHome/copyDotFiles_fromgitRepo_to_home.sh
+++ dotFilesHome/copyDotFiles_fromgitRepo_to_home.sh
@@ -5,5 +5,5 @@ sudo cp .bashrc_backup ~/.bashrc_backup
 sudo cp .vimrc ~/.vimrc
 sudo cp -r .vim ~/.vim
 sudo cp .tmux.conf ~/.tmux.conf
-sudo cp -r /config ~/.config
+sudo cp -r .config ~/.config
 
